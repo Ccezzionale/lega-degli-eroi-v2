2638e7ba640e5949d857652600f9a6245733bc8a
Ccezzionale <tringali0511@gmail.com>
2025-06-08 22:25:15 +0200

    Add files via upload

---
+++ rose.js
@@ -0,0 +1,16 @@
+const rose = {
+  "Team Bartowski": [],
+  "Rubin Kebab": [],
+  "wildboys78": [],
+  "Desperados": [],
+  "MinneSota Snakes": [],
+  "PokerMantra": [],
+  "Bayern Christiansen": [],
+  "Minnesode Timberland": [],
+  "Sharknado 04": [],
+  "Real Mimmo": [],
+  "Giody": [],
+  "Ibla": [],
+  "Pandinicoccolosini": [],
+  "Giulay": []
+};
