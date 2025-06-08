017dd87178769badb3223abd938c3fac84a639a5
Ccezzionale <tringali0511@gmail.com>
2025-06-08 22:31:55 +0200

    Add files via upload

---
+++ rose.js
@@ -0,0 +1,166 @@
+const rose = {
+  "Team Bartowski": [
+    {
+      "ruolo": "Por",
+      "nome": "Reina",
+      "squadra": "Com",
+      "costo": 4
+    },
+    {
+      "ruolo": "Por",
+      "nome": "Meret",
+      "squadra": "Nap",
+      "costo": 19
+    },
+    {
+      "ruolo": "Por",
+      "nome": "Scuffet",
+      "squadra": "Nap",
+      "costo": 2
+    },
+    {
+      "ruolo": "Por",
+      "nome": "Vigorito",
+      "squadra": "Com",
+      "costo": 1
+    },
+    {
+      "ruolo": "Por",
+      "nome": "Contini",
+      "squadra": "Nap",
+      "costo": 1
+    },
+    {
+      "ruolo": "Por",
+      "nome": "Butez",
+      "squadra": "Com",
+      "costo": 9
+    },
+    {
+      "ruolo": "E",
+      "nome": "Dimarco",
+      "squadra": "Int",
+      "costo": 21
+    },
+    {
+      "ruolo": "E",
+      "nome": "Zappacosta",
+      "squadra": "Ata",
+      "costo": 15
+    },
+    {
+      "ruolo": "B;Ds;E",
+      "nome": "Kolasinac",
+      "squadra": "Ata",
+      "costo": 7
+    },
+    {
+      "ruolo": "Dd;Dc",
+      "nome": "Pavard",
+      "squadra": "Int",
+      "costo": 7
+    },
+    {
+      "ruolo": "Ds;E",
+      "nome": "Hernandez T.",
+      "squadra": "Mil",
+      "costo": 14
+    },
+    {
+      "ruolo": "Dc",
+      "nome": "Maripan",
+      "squadra": "Tor",
+      "costo": 9
+    },
+    {
+      "ruolo": "Ds;Dc",
+      "nome": "Vasquez",
+      "squadra": "Gen",
+      "costo": 10
+    },
+    {
+      "ruolo": "Dd;Ds;E",
+      "nome": "Birindelli",
+      "squadra": "Mon",
+      "costo": 10
+    },
+    {
+      "ruolo": "E",
+      "nome": "Carlos Augusto",
+      "squadra": "Int",
+      "costo": 18
+    },
+    {
+      "ruolo": "C",
+      "nome": "Mkhitaryan",
+      "squadra": "Int",
+      "costo": 13
+    },
+    {
+      "ruolo": "M;C",
+      "nome": "Lobotka",
+      "squadra": "Nap",
+      "costo": 10
+    },
+    {
+      "ruolo": "W;T",
+      "nome": "Elmas",
+      "squadra": "Tor",
+      "costo": 13
+    },
+    {
+      "ruolo": "M;C",
+      "nome": "McKennie",
+      "squadra": "Juv",
+      "costo": 12
+    },
+    {
+      "ruolo": "C;T",
+      "nome": "Miretti",
+      "squadra": "Gen",
+      "costo": 12
+    },
+    {
+      "ruolo": "T;A",
+      "nome": "Baldanzi",
+      "squadra": "Rom",
+      "costo": 8
+    },
+    {
+      "ruolo": "M;C",
+      "nome": "Makoumbou",
+      "squadra": "Cag",
+      "costo": 5
+    },
+    {
+      "ruolo": "T;A",
+      "nome": "Paz N.",
+      "squadra": "Com",
+      "costo": 22
+    },
+    {
+      "ruolo": "W;A",
+      "nome": "Conceicao",
+      "squadra": "Juv",
+      "costo": 15
+    },
+    {
+      "ruolo": "Pc",
+      "nome": "Martinez L.",
+      "squadra": "Int",
+      "costo": 28
+    },
+    {
+      "ruolo": "Pc",
+      "nome": "Retegui",
+      "squadra": "Ata",
+      "costo": 38
+    },
+    {
+      "ruolo": "A",
+      "nome": "Dominguez B.",
+      "squadra": "Bol",
+      "costo": 20
+    }
+  ]
+};
