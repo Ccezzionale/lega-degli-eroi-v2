1b141fe3677a04216f1af3032bb962d1256d9dec
Ccezzionale <tringali0511@gmail.com>
2025-06-08 22:15:00 +0200

    Add files via upload

---
+++ rose.js
@@ -0,0 +1,168 @@
+const rose = {
+  "Team Bartowski": [
+    {
+      "nome": "Reina",
+      "status": "fp"
+    },
+    {
+      "nome": "Vigorito",
+      "status": "fp"
+    },
+    {
+      "nome": "Butez",
+      "status": "fp"
+    },
+    {
+      "nome": "Kolasinac",
+      "status": "fp"
+    },
+    {
+      "nome": "Pavard",
+      "status": "fp"
+    },
+    {
+      "nome": "Maripan",
+      "status": "fp"
+    },
+    {
+      "nome": "Mkhitaryan",
+      "status": "fp"
+    },
+    {
+      "nome": "Lobotka",
+      "status": "fp"
+    },
+    {
+      "nome": "Elmas",
+      "status": "fp"
+    },
+    {
+      "nome": "McKennie",
+      "status": "fp"
+    },
+    {
+      "nome": "Miretti",
+      "status": "fp"
+    },
+    {
+      "nome": "Makoumbou",
+      "status": "fp"
+    },
+    {
+      "nome": "Baldanzi",
+      "status": "u21"
+    }
+  ],
+  "Rubin Kebab": [
+    {
+      "nome": "Reina",
+      "status": "fp"
+    },
+    {
+      "nome": "Vigorito",
+      "status": "fp"
+    },
+    {
+      "nome": "Butez",
+      "status": "fp"
+    },
+    {
+      "nome": "Kristensen T.",
+      "status": "fp"
+    },
+    {
+      "nome": "Zielinski",
+      "status": "fp"
+    },
+    {
+      "nome": "Freuler",
+      "status": "fp"
+    },
+    {
+      "nome": "Ilic",
+      "status": "fp"
+    },
+    {
+      "nome": "Da Cunha",
+      "status": "fp"
+    },
+    {
+      "nome": "Frendrup",
+      "status": "fp"
+    },
+    {
+      "nome": "Oristanio",
+      "status": "fp"
+    },
+    {
+      "nome": "Folorunsho",
+      "status": "fp"
+    },
+    {
+      "nome": "De Ketelaere",
+      "status": "fp"
+    }
+  ],
+  "wildboys78": [
+    {
+      "nome": "Provedel",
+      "status": "fp"
+    },
+    {
+      "nome": "Furlanetto",
+      "status": "fp"
+    },
+    {
+      "nome": "Mandas",
+      "status": "fp"
+    },
+    {
+      "nome": "Acerbi",
+      "status": "fp"
+    },
+    {
+      "nome": "Walker",
+      "status": "fp"
+    },
+    {
+      "nome": "Zappa",
+      "status": "fp"
+    },
+    {
+      "nome": "Thiaw",
+      "status": "fp"
+    },
+    {
+      "nome": "Freuler",
+      "status": "fp"
+    },
+    {
+      "nome": "Guendouzi",
+      "status": "fp"
+    },
+    {
+      "nome": "Ricci S.",
+      "status": "fp"
+    },
+    {
+      "nome": "Ramadani",
+      "status": "fp"
+    },
+    {
+      "nome": "Ekkelenkamp",
+      "status": "fp"
+    },
+    {
+      "nome": "Isaksen",
+      "status": "fp"
+    },
+    {
+      "nome": "Bonny",
+      "status": "fp"
+    },
+    {
+      "nome": "Tchaouna",
+      "status": "u21"
+    }
+  ]
+};
