5186a28c82ede57df8d2159c1ac0051758329187
Ccezzionale <tringali0511@gmail.com>
2025-06-09 12:49:57 +0200

    Add files via upload

---
+++ albo_doro.js
@@ -0,0 +1,60 @@
+export const alboDoro = {
+  "2024-2025": {
+    "Campionato": [
+      "Bayern Christiansen",
+      "Real Mimmo",
+      "Ibla"
+    ],
+    "Highlander Cup": [
+      "Rubinkebab",
+      "Team Bartowski",
+      "Bayern Christiansen"
+    ],
+    "Conference Championship": [
+      "Team Bartowski",
+      "Rubinkebab",
+      "Union Librino"
+    ],
+    "Round Robin": [
+      "Team Bartowski",
+      "MinnesodeTimberland",
+      "Bayern Christiansen"
+    ]
+  },
+  "2023-2024": {
+    "Campionato": [
+      "Costantinobull",
+      "Pollisti",
+      "Bayern Christiansen"
+    ],
+    "Highlander Cup": [
+      "Costantinobull",
+      "Pollisti",
+      "POKERMANTRA"
+    ],
+    "Conference Championship": [
+      "Real Mimmo",
+      "Desperados",
+      "Pollisti"
+    ],
+    "Round Robin": [
+      "MinneSota Snakes",
+      "3 Amici al Var",
+      "Team Bartowski"
+    ]
+  },
+  "2022-2023": {
+    "Campionato": [
+      "Pollisti",
+      "Cugini di Zampagna",
+      "Desperados/ Real Mimmo"
+    ],
+    "Highlander Cup": [
+      "Pollisti",
+      "Desperados",
+      "Cugini di Zampagna"
+    ],
+    "Conference Championship": [],
+    "Round Robin": []
+  }
+};
